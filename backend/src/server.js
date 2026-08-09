ec0cbb62160982d9d5b52b0fa97d1bb1901ada40
Renan <renanoruim8@gmail.com>
2026-08-08 21:59:01 -0300

    Adicioando o .env para segurança do deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ app.use("/api", authRouter);
 
 app.use("/api", noticiasRouter);
 
-const PORTA = 3000;
+const PORTA = process.env.PORT || 3000;
 
 app.listen(PORTA, () => {
   console.log(`Servidor rodando em http://localhost:${PORTA}`);
